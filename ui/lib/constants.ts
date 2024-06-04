b0475e5d66a2a8d1371056df9a3f0ad75b1b4d99
ross <92001561+z0r0z@users.noreply.github.com>
2024-06-04 08:29:42 +0000

    🖥️ UI

--- ui/lib/constants.ts
+++ ui/lib/constants.ts
@@ -4,7 +4,7 @@ export const IE_ADDRESS: {
   [key: number]: Address;
 } = {
   1: "0x1E00000000Cf8ba83e0005c59c1Bf1C4682C8E00",
-  42161: "0x1e00003a669bb466d6B49800000099E1abDD6600",
+  42161: "0x1e00002C59149d0057F12e031ecC0000d38A000C",
 };
 
 export const ETH_ADDRESS = "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE";
